a222c0fdfdd6ad6a0465ea9ca5385e63e14eff01
vinithacsekite <vinithavini055@gmail.com>
2018-11-27 14:34:59 +0530

    Addressbook with aoracle

---
+++ src/mvc/models/Person.java
@@ -0,0 +1,65 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mvc.models;
+
+/**
+ *
+ * @author Admin
+ */
+public class Person {
+    private String name;
+    private String mob;
+    private String email;
+    private String gender;
+    private String fb;
+    
+    
+   /* public User(){
+        x=0;
+    }
+    
+    public User(int x){
+        this.x=x;
+    }
+    
+    public void incX(){
+        x++;
+    }
+    
+    public void decX(){
+        x--;
+    }
+    public int getX(){
+        return x;
+    }*/
+    public String getName(){
+        return this.name; 
+   }
+    
+    public String getMob(){
+        return this.mob; 
+   }
+    
+    public String getEmail(){
+        return this.email; 
+   }
+    public String getGender(){
+        return this.gender;
+    }
+      public String getFb(){
+        return this.fb;
+    }
+    
+    public void setData(String name, String mob,String email,String gender,String fb){
+        this.name = name;
+        this.mob = mob;
+        this.email = email;
+        this.gender=gender;
+        this.fb=fb;
+    }
+    
+ 
+
+}
